035727740f4fc57c6a19d4fcfbd3b2c53b3e8125
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 16:55:27 +0530

    Add suqares, modules

--- Python/functions.py
+++ Python/functions.py
@@ -1,6 +1,3 @@
 # Function in python
 def square(x):
     return x * x
-
-for i in range(10):
-    print(f"The square of {i} is {square(i)}")
---
+++ Python/squares.py
@@ -0,0 +1,5 @@
+# Importing functions module from functions.py
+from functions import square
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
