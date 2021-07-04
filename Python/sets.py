09b825a12418c4df5b368565e5ba5bc816d3042a
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 15:34:04 +0530

    Add sets

---
+++ Python/sets.py
@@ -0,0 +1,18 @@
+# Create an empty set
+# Set contains unique elements
+s = set()
+
+# Add elements
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+print(s)
+
+# Remove elements
+s.remove(2)
+print(s)
+
+# Length of set
+print(f"The set has {len(s)} elements.")
