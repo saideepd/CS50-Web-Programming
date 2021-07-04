4d2603c2a753e3bbe049ceec27f8e52ac022140a
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 15:06:41 +0530

    Conditions & Typecasting

---
+++ Python/conditions.py
@@ -0,0 +1,9 @@
+#Conditions & Typecasting
+n = float(input("Number: "))
+
+if n > 0:
+    print("Number is positive")
+elif n < 0:
+    print("Number is negative")
+else:
+    print("Number is zero")
