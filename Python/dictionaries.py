c61ea534b8c509bbc24a2953d84ea92fb4f8e3ca
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 16:39:18 +0530

    Add dictionaries

---
+++ Python/dictionaries.py
@@ -0,0 +1,8 @@
+# Dictionaries in Key-Value pairs
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+# Print value for Key
+print(houses["Harry"])
+
+# Add Key-Value pair to a dictionary
+houses["Hermoine"] = "Gryffindor"
+print(houses["Hermoine"])
