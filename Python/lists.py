451f05c18607b9f555e2a3f6fb70eb74585ca565
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 15:27:49 +0530

    Add lists

---
+++ Python/lists.py
@@ -0,0 +1,12 @@
+# Define a List of names
+names = ["Sam", "Bucky", "Zemo", "Sharon"]
+print(names)
+print(names[0])
+
+# Append element at end of list
+names.append("John")
+print(names)
+
+# Sort list
+names.sort()
+print(names)
