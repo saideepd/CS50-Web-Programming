1bcd33c10b1a30888f9ab75d6754bf5f18a122f9
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 14:58:51 +0530

    Python Variables, Formatted Strings, Datatypes

---
+++ Python/name.py
@@ -0,0 +1,20 @@
+#Variables
+name = input("Name: ")
+print("Hello, " + name)
+
+# Formatted Strings
+# This is similar to String Interpolation in C# & JS
+print(f"Hello, {name}")
+
+# Datatypes
+a = 5 #int
+b = 2.5 #float
+c = "India" #str
+d = True #bool
+e = None #NoneType
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
