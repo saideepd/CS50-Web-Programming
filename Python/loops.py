afad08b9c53050d05de6fa22612b7f3a81a21704
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 16:33:42 +0530

    Add loops

---
+++ Python/loops.py
@@ -0,0 +1,21 @@
+# Loop in a list
+print("Loop in a list:")
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+# Loop in a range
+print("Loop in a range:")
+for i in range(6):
+    print(i)
+    
+# Loop list of strings
+print("Loop list of strings:")
+names = ["Sam", "Bucky", "Zemo", "Sharon"]
+for name in names:
+    print(name)
+    
+# Loop characters of a strings
+print("Loop characters of a strings:")
+name = "Harry"
+for character in name:
+    print(character)
