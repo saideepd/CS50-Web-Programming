058a8bd51e54ad4714acf6a73410771b8f777d6d
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-04 15:16:56 +0530

    Add sequences

---
+++ Python/sequences.py
@@ -0,0 +1,14 @@
+# String sequences
+name = "Harry"
+print(name[1])
+
+# List
+names = ["Sam", "Bucky", "Zemo", "Sharon"]
+print(names)
+print(names[0:2])
+
+# Tuples
+coordinateX = 10.0
+coordinateY = 20.0
+coordinate = (10.0, 20.0)
+print(coordinate)
