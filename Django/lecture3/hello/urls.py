5f319a177996e09a80d2f2b51a28b5bca5d72ee0
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-07 01:14:56 +0530

    Added new routes & views

--- Django/lecture3/hello/urls.py
+++ Django/lecture3/hello/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("saideep", views.saideep, name="saideep"),
+    path("dicholkar", views.dicholkar)
 ]
--- Django/lecture3/hello/views.py
+++ Django/lecture3/hello/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello!")
+
+def saideep(request):
+    return HttpResponse("Hello, Saideep!")
+
+def dicholkar(request):
+    return HttpResponse("Hello, Mr. Dicholkar!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
