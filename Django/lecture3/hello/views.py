afedfa5a3a5df41c2d30ef953929af2980d0e97d
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-07 22:51:10 +0530

    Escape Input to address security issue

--- Django/lecture3/hello/views.py
+++ Django/lecture3/hello/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import html
 
 # Create your views here.
 def index(request):
@@ -12,4 +13,4 @@ def dicholkar(request):
     return HttpResponse("Hello, Mr. Dicholkar!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return HttpResponse(f"Hello, {html.escape(name.capitalize())}!")
