1110d62e192e3471251ccd55520a4d6d8329ef8d
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-07-08 23:47:20 +0530

    Added Templates for Task app, made templates dynamic

--- Django/lecture3/tasks/views.py
+++ Django/lecture3/tasks/views.py
@@ -6,4 +6,7 @@ tasks = ["foo", "bar", "baz", "sam"]
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
-    }) 
+    })
+
+def add(request):
+    return render(request, "tasks/add.html")
